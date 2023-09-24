beda54410c96766b031c665d593337db3aa85973
leeool <lgonsalez02@gmail.com>
2023-09-24 16:16:53 -0300

    feat: update footer & images

--- src/interfaces/Footer/Footer.tsx
+++ src/interfaces/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <S.Container>
       <p>
-        Este site foi desenvolvido por <a href="https://www.linkedin.com/in/leonardo-gonsalez/" rel="external" target='_blank'>Leonardo Gonsalez</a> e <a href="https://www.instagram.com/wassup4bel/" rel='external' target='_blank'>Isabel de Jesus</a></p>
+        Este site foi desenvolvido pelos estudantes <a href="https://www.linkedin.com/in/leonardo-gonsalez/" rel="external" target='_blank'>Leonardo Gonsalez (3MA2)</a> e <a href="https://www.instagram.com/wassup4bel/" rel='external' target='_blank'>Isabel de Jesus (3MA2)</a></p>
     </S.Container>
   )
 }
