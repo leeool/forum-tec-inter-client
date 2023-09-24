fbacc9ab2feeb5c5c22fd976bc72081a16728e73
leeool <lgonsalez02@gmail.com>
2023-09-24 01:08:15 -0300

    feat: update server url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://quaint-dog-uniform.cyclic.app/",
+  baseURL: "https://quaint-dog-uniform.cyclic.app",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
